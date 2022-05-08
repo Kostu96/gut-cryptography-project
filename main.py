0bf96784c0ba322d23aa4fcbdd1cbe8d59a3dfd3
iwanicki92 <iwans16@gmail.com>
2022-05-09 00:51:31 +0200

    Rebased to current version

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-<<<<<<< HEAD
 from PyQt5.QtWidgets import QApplication
 from ui import UI
-
-
-if __name__ == '__main__':
-    app = QApplication([])
-    mainWindow = UI()
-    app.exec()
-=======
-from gui import CryptoGUI
-from wdc_crypto import *
 from argparse import ArgumentParser
+from crypto import *
+from time import process_time
 
 def parseArgs() -> ArgumentParser:
     parser = ArgumentParser(description='Program do generowania kluczy asymetrycznych oraz szyfrowania i deszyfrowania plików')
     subparser = parser.add_subparsers(help='sub-command help', dest='command')
 
     g_parser: ArgumentParser = subparser.add_parser('generate-keys', help='Generuj klucze asymetryczne')
-    g_parser.add_argument('type', choices=AssymetricKeyType._member_names_, help='Typ algorytmu')
-    g_parser.add_argument('length', type=int, choices=[16,32,64,128,256,512,1024], help='Dlugosc klucza')
+    g_parser.add_argument('length', type=int, choices=[1024, 2048, 4096, 8192], help='Dlugosc klucza w bitach')
 
     return parser
 
 if __name__ == "__main__":
     parser = parseArgs()
     args = vars(parser.parse_args())
-    print(args)
+    #print(args) # debug
 
     if args['command'] is None:
-        app = CryptoGUI()
-        app.run()
+        app = QApplication([])
+        mainWindow = UI()
+        app.exec()
     elif args['command'] == 'generate-keys':
-        private, public = generateKeys(AssymetricKeyType[args['type']], args['length'])
-        print('Private key:'.ljust(12), private.key)
-        print('Public key:'.ljust(12), public.key)
->>>>>>> 64ba188 (example of possible code)
+        public, private = generate_keys(args['length'])
