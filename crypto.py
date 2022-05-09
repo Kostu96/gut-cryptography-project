b7946b9109401180956cbe65ec4746d7fcacbc70
iwanicki92 <iwans16@gmail.com>
2022-05-09 21:58:35 +0200

    small fixes after rebase

--- crypto.py
+++ crypto.py
@@ -1,13 +1,13 @@
 import rsa
 
 
-def generate_keys(length: int = 1024):
+def generate_keys(length: int = 1024) -> tuple[str, str]:
     """
-    :param int bits: number of bits used to generate keys.
+    :param int length: number of bits used to generate keys.
     :returns: a pair of string with public and private key in that order.
     """
 
-    publicKeyRaw, privateKeyRaw = rsa.newkeys(bits, True, 4)
+    publicKeyRaw, privateKeyRaw = rsa.newkeys(nbits=length, accurate=True, poolsize=4)
     publicKeyStr = publicKeyRaw.save_pkcs1("PEM").decode("utf-8")
     privateKeyStr = privateKeyRaw.save_pkcs1("PEM").decode("utf-8")
 
