58979d190fa2fbd6fa50c5c22eb522c3ca68028c
Konstanty <Kostu96@gmail.com>
2022-05-09 21:35:55 +0200

    Add UI for simple encoding/decoding demo

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QPushButton
+from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
 from crypto import *
 
@@ -9,16 +9,21 @@ class UI(QMainWindow):
 
         uic.loadUi("application.ui", self)
 
-        genKeysBtn = self.findChild(QPushButton, "genKeysBtn")
-        genKeysBtn.clicked.connect(self.gen_keys_btn_clicked)
-
         self.bitsComboBox.addItems(["512", "1024", "2048", "4096"])
+        self.genKeysBtn.clicked.connect(self.gen_keys_btn_clicked)
+        self.encodeBtn.clicked.connect(self.encode_btn_clicked)
+        self.decodeBtn.clicked.connect(self.decode_btn_clicked)
 
         self.show()
 
     def gen_keys_btn_clicked(self):
         bits = int(self.bitsComboBox.currentText())
         [pub_key, priv_key] = generate_keys(bits)
-        print(pub_key, '\n', priv_key)
         self.pubKeyTextEdit.setPlainText(pub_key)
         self.privKeyTextEdit.setPlainText(priv_key)
+
+    def encode_btn_clicked(self):
+        pass
+
+    def decode_btn_clicked(self):
+        pass
